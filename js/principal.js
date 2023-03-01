c051b4ce8d8c729d97366168f36711b9340befc9
Lenin <116517200+lenin2499@users.noreply.github.com>
2023-02-28 18:32:01 -0600

    pagina completa - falta: cv, cursos hobbies y formularios.

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,13 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+
+window.addEventListener("scroll", function(){
+    movimiento();
+})
+
+function movimiento() {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("scrolling", window.scrollY > 20);
+}
