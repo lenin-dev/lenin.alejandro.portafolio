2ca12b155be0f428cd1686226b4e8df8c8ca3a5d
Lenin <lads2499@gmail.com>
2023-05-03 15:05:04 -0600

    ocultar proyecto sesiones

--- js/principal.js
+++ js/principal.js
@@ -27,59 +27,64 @@ function carga() {
             var cartas = document.getElementById('cartas');
 
             for(let i in arreglo) {
-                var div = document.createElement('div');
-                    div.setAttribute("class", "card");
 
-                    var div1 = document.createElement('div');
-                        div1.setAttribute("class", "img");
-                    var img = document.createElement('img');
-                        img.setAttribute("id", "img-icon");
-                        img.setAttribute("class", "imagen-icon-portafolio");
-                        img.setAttribute("src", arreglo[i].icono);
-                    div1.appendChild(img);
+                if(arreglo[i].clave == "") {
 
-                    var span = document.createElement('span');
-                        var textValor1 = document.createTextNode(arreglo[i].nombre);
-                        span.appendChild(textValor1);
+                } else {
+                    var div = document.createElement('div');
+                        div.setAttribute("class", "card");
 
-                    var p = document.createElement('p');
-                        p.setAttribute("class", "info");
-                        var textValor2 = document.createTextNode(arreglo[i].description);
-                        p.appendChild(textValor2);
+                        var div1 = document.createElement('div');
+                            div1.setAttribute("class", "img");
+                        var img = document.createElement('img');
+                            img.setAttribute("id", "img-icon");
+                            img.setAttribute("class", "imagen-icon-portafolio");
+                            img.setAttribute("src", arreglo[i].icono);
+                        div1.appendChild(img);
 
-                    var div2 = document.createElement('div');
-                        div2.setAttribute("class", "share");
-                        var a = document.createElement('a');
-                            a.setAttribute("href", arreglo[i].pagina);
-                            a.setAttribute("target", "_blank");
-                            var img2 = document.createElement('img');
-                            img2.setAttribute("src", "img/icon/monitor.svg");
-                            img2.setAttribute("class", "icon-svg-portafolio");
-                        var a2 = document.createElement('a');
-                            a2.setAttribute("href", arreglo[i].github);
-                            a2.setAttribute("target", "_blank");
-                            var img3 = document.createElement('img');
-                            img3.setAttribute("src", "img/icon/github.svg");
-                            img3.setAttribute("class", "icon-svg-portafolio");
-                                a.appendChild(img2);
-                                a2.appendChild(img3);
-                                div2.appendChild(a);
-                                div2.appendChild(a2);
-                var button = document.createElement('button');
-                    button.setAttribute("data-bs-toggle", "modal");
-                    button.setAttribute("data-bs-target", "#exampleModal");
-                    button.setAttribute("value", arreglo[i].clave);
-                    button.setAttribute("onclick", "abrirModal(value)" );
-                    var textValor3 = document.createTextNode("resumen");
-                    button.appendChild(textValor3);
+                        var span = document.createElement('span');
+                            var textValor1 = document.createTextNode(arreglo[i].nombre);
+                            span.appendChild(textValor1);
 
-                div.appendChild(div1);
-                div.appendChild(span);
-                div.appendChild(p);
-                div.appendChild(div2);
-                div.appendChild(button);
-                cartas.appendChild(div);
-                // div.appendChild();
+                        var p = document.createElement('p');
+                            p.setAttribute("class", "info");
+                            var textValor2 = document.createTextNode(arreglo[i].description);
+                            p.appendChild(textValor2);
+
+                        var div2 = document.createElement('div');
+                            div2.setAttribute("class", "share");
+                            var a = document.createElement('a');
+                                a.setAttribute("href", arreglo[i].pagina);
+                                a.setAttribute("target", "_blank");
+                                var img2 = document.createElement('img');
+                                img2.setAttribute("src", "img/icon/monitor.svg");
+                                img2.setAttribute("class", "icon-svg-portafolio");
+                            var a2 = document.createElement('a');
+                                a2.setAttribute("href", arreglo[i].github);
+                                a2.setAttribute("target", "_blank");
+                                var img3 = document.createElement('img');
+                                img3.setAttribute("src", "img/icon/github.svg");
+                                img3.setAttribute("class", "icon-svg-portafolio");
+                                    a.appendChild(img2);
+                                    a2.appendChild(img3);
+                                    div2.appendChild(a);
+                                    div2.appendChild(a2);
+                    var button = document.createElement('button');
+                        button.setAttribute("data-bs-toggle", "modal");
+                        button.setAttribute("data-bs-target", "#exampleModal");
+                        button.setAttribute("value", arreglo[i].clave);
+                        button.setAttribute("onclick", "abrirModal(value)" );
+                        var textValor3 = document.createTextNode("resumen");
+                        button.appendChild(textValor3);
+
+                    div.appendChild(div1);
+                    div.appendChild(span);
+                    div.appendChild(p);
+                    div.appendChild(div2);
+                    div.appendChild(button);
+                    cartas.appendChild(div);
+                    // div.appendChild();
+                }
 
             }
         }
